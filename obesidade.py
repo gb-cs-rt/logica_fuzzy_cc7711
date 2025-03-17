8f33dad9c52d804e8069323347ec774fb53e823c
Baggiio <gustavo.gbaggio@gmail.com>
2025-03-16 23:24:29 -0300

    updated to allow more tests

--- obesidade.py
+++ obesidade.py
@@ -6,50 +6,106 @@ import matplotlib.pyplot as plt
 from skfuzzy import control as ctrl
 
 #Variaveis de Entrada (Antecedent)
-comer = ctrl.Antecedent(np.arange(0, 8, 1), 'comer')
-tempoAtvFisica = ctrl.Antecedent(np.arange(0, 8, 1), 'tempoAtvFisica')
+comer = ctrl.Antecedent(np.arange(0, 12, 1), 'comer') # 1000 kcal
+tempoAtvFisica = ctrl.Antecedent(np.arange(0, 240, 1), 'tempoAtvFisica') # minutos
 
-#Variaveis de saída (Consequent)
-peso = ctrl.Consequent(np.arange(0, 10, 1), 'peso')
+# Variaveis de saída (Consequent)
+peso = ctrl.Consequent(np.arange(0, 14, 1), 'peso')
 
-# automf -> Atribuição de categorias automaticamente
-comer.automf(names=['pouco','razoavel','bastante'],)
-tempoAtvFisica.automf(names=['pouco tempo', 'tempo medio', 'muito tempo'])
+# Funções de pertinência
 
-# atribuicao sem o automf
-peso['peso leve'] = fuzz.trapmf(peso.universe, [-1, 0, 4, 6])
-peso['peso medio'] = fuzz.trapmf(peso.universe, [4, 6, 8, 10])
-peso['pesado'] = fuzz.trapmf(peso.universe, [8, 10, 12, 14])
+# Tempo de Atividade Física
+tipo_atv_fisica = "triangular"
+
+if tipo_atv_fisica == "triangular":
+    tempoAtvFisica.automf(names=['pouco tempo', 'tempo medio', 'muito tempo'])
+elif tipo_atv_fisica == "trapezoidal":
+    tempoAtvFisica['pouco tempo'] = fuzz.trapmf(tempoAtvFisica.universe, [0, 0, 30, 60])
+    tempoAtvFisica['tempo medio'] = fuzz.trapmf(tempoAtvFisica.universe, [30, 60, 120, 150])
+    tempoAtvFisica['muito tempo'] = fuzz.trapmf(tempoAtvFisica.universe, [120, 150, 240, 240])
+elif tipo_atv_fisica == "gaussiana":
+    tempoAtvFisica['pouco tempo'] = fuzz.gaussmf(tempoAtvFisica.universe, 0, 20)
+    tempoAtvFisica['tempo medio'] = fuzz.gaussmf(tempoAtvFisica.universe, 90, 20)
+    tempoAtvFisica['muito tempo'] = fuzz.gaussmf(tempoAtvFisica.universe, 240, 20)
+
+# Comer
+tipo_comer = "trapezoidal"
+
+if tipo_comer == "triangular":
+    comer.automf(names=['pouco', 'razoavel', 'bastante'])
+elif tipo_comer == "trapezoidal":
+    comer['pouco'] = fuzz.trapmf(comer.universe, [0, 0, 2, 4])
+    comer['razoavel'] = fuzz.trapmf(comer.universe, [2, 4, 12, 12])
+    comer['bastante'] = fuzz.trapmf(comer.universe, [4, 6, 12, 12])
+elif tipo_comer == "gaussiana":
+    comer['pouco'] = fuzz.gaussmf(comer.universe, 0, 2)
+    comer['razoavel'] = fuzz.gaussmf(comer.universe, 6, 2)
+    comer['bastante'] = fuzz.gaussmf(comer.universe, 12, 2)
+
+# Peso
+tipo_peso = "trapezoidal"
+
+if tipo_peso == "triangular":
+    peso.automf(names=['peso leve', 'peso medio', 'pesado'])
+elif tipo_peso == "trapezoidal":
+    peso['peso leve'] = fuzz.trapmf(peso.universe, [0, 0, 4, 6])
+    peso['peso medio'] = fuzz.trapmf(peso.universe, [4, 6, 8, 10])
+    peso['pesado'] = fuzz.trapmf(peso.universe, [8, 10, 14, 14])
+elif tipo_peso == "gaussiana":
+    peso['peso leve'] = fuzz.gaussmf(peso.universe, 0, 2)
+    peso['peso medio'] = fuzz.gaussmf(peso.universe, 6, 2)
+    peso['pesado'] = fuzz.gaussmf(peso.universe, 12, 2)
 
 #Visualizando as variáveis
 comer.view()
 tempoAtvFisica.view()
 peso.view()
 
-#Criando as regras
-regra_1 = ctrl.Rule(comer['bastante'] | tempoAtvFisica['pouco tempo'], peso['pesado'])
-regra_2 = ctrl.Rule(comer['razoavel'] & tempoAtvFisica['tempo medio'], peso['peso medio'])
-regra_3 = ctrl.Rule(comer['pouco'] & tempoAtvFisica['muito tempo'], peso['peso leve'])
-regra_4 = ctrl.Rule(comer['pouco'] & tempoAtvFisica['pouco tempo'], peso['peso leve'])
-regra_5 = ctrl.Rule(comer['bastante'] & tempoAtvFisica['muito tempo'], peso['pesado'])
+# Usar tempo de atividade física
+usar_tempo_atv_fisica = True
 
-controlador = ctrl.ControlSystem([regra_1, regra_2, regra_3])
+if not usar_tempo_atv_fisica:
+
+    # Criando as regras (somente uma variável de entrada)
+    regra_1_1 = ctrl.Rule(comer['bastante'], peso['pesado'])
+    regra_1_2 = ctrl.Rule(comer['razoavel'], peso['peso medio'])
+    regra_1_3 = ctrl.Rule(comer['pouco'], peso['peso leve'])
+    controlador = ctrl.ControlSystem([regra_1_1, regra_1_2, regra_1_3])
+
+else:
+
+    # Criando as regras (duas variáveis de entrada)
+    regra_2_1 = ctrl.Rule(comer['bastante'] | tempoAtvFisica['pouco tempo'], peso['pesado'])
+    regra_2_2 = ctrl.Rule(comer['razoavel'] & tempoAtvFisica['tempo medio'], peso['peso medio'])
+    regra_2_3 = ctrl.Rule(comer['pouco'] & tempoAtvFisica['muito tempo'], peso['peso leve'])
+    regra_2_4 = ctrl.Rule(comer['pouco'] & tempoAtvFisica['pouco tempo'], peso['peso leve'])
+    regra_2_5 = ctrl.Rule(comer['bastante'] & tempoAtvFisica['muito tempo'], peso['peso medio'])
+    controlador = ctrl.ControlSystem([regra_2_1, regra_2_2, regra_2_3, regra_2_4, regra_2_5])
 
 #Simulando
 CalculoPeso = ctrl.ControlSystemSimulation(controlador)
 
 notaPeso = int(input('Comer: '))
-notaTempoAtvFisica = int(input('Tempo de Atividade Física: '))
 CalculoPeso.input['comer'] = notaPeso
-CalculoPeso.input['tempoAtvFisica'] = notaTempoAtvFisica
+
+if usar_tempo_atv_fisica:
+    notaTempoAtvFisica = int(input('Tempo de Atividade Física: '))
+    CalculoPeso.input['tempoAtvFisica'] = notaTempoAtvFisica
+
 CalculoPeso.compute()
 
 valorPeso = CalculoPeso.output['peso']
 
-print("\n Comer %d \nPeso %5.2f \n Tempo de Atividade Física %d \n" % (notaPeso, valorPeso, notaTempoAtvFisica))
+if not usar_tempo_atv_fisica:
+    print("\nComer: %d \nPeso: %5.2f\n" % (notaPeso, valorPeso))
+else:
+    print("\nComer: %d\nTempo de Atividade Física: %d\nPeso: %5.2f \n" % (notaPeso, notaTempoAtvFisica, valorPeso))
 
 
 comer.view(sim=CalculoPeso)
 peso.view(sim=CalculoPeso)
+
+if usar_tempo_atv_fisica:
+    tempoAtvFisica.view(sim=CalculoPeso)
 
 plt.show()
